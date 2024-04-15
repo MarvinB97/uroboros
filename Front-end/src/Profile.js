f827f41c3b39ffc859a7767ec5af498015653993
MarvinB97 <103372738+MarvinB97@users.noreply.github.com>
2024-04-14 21:14:25 -0500

    Update Profile.js

--- Front-end/src/Profile.js
+++ Front-end/src/Profile.js
@@ -1,4 +1,10 @@
+/*
+*------------------LIBRERIAS Y ARCHIVOS EXTERNOS-----------------
+*/
 import {usuariosList} from './UsuariosLista.js';
+import ListaElementos from './ListaElementos.js';
+import Encabezado from './Encabezado.js';
+import {usu} from './Login.js';
 
 import React, { useState } from 'react';
 import { Collapse, Button, CardBody, Card } from 'reactstrap';
@@ -6,48 +12,29 @@ import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem} from 'reactstrap'
 
 import { useNavigate } from 'react-router-dom';
 
-import star from './star.png';
-import logo from './logo2.png';
-import {usu} from './Login.js';
 
 
-//import { datos } from './Login.js';
-
-
-export default function Profile(){//
-    let n=10;//longitud de la lista
-    const description = [[],[]];
-
-    for(let i=0;i<2; i++){
-
-        const des = [];
-
-        for(let j=0;j<n; j++){
-            des.push('elemento '+ (j+1) +' de la lista \n'); 
-        }
-        description[i] = des;
-    }
-
+/*
+*------------------FUNCION PRINCIPAL-----------------
+*/
+export default function Profile(){
 
     let title =['Lista de Miembros', 'Lista de Obras'];
-
     const usur = usu;
     
     return (
         <div>
-            <header className='App-header'>
-                <ProfileHeader/>
-                <hr/>
-            </header>
+            <Encabezado className='Encabezado' boton={<Options/>}/>
+
             <div className='flexbox-container-profile'>
                 <div className='column-one-profile'>
                     <ProfileColumnOne info={usur}/>
                 </div>
                 <div className='column-two-profile'>
-                    <ButtonBar title={title[0]} description={description[0]}/>
+                    <ButtonBar title={title[0]} elementos={<ListaElementos/>}/>
                 </div>
                 <div className='column-three-profile'>
-                    <ButtonBar title={title[1]} description={description[1]}/>
+                    <ButtonBar title={title[1]} elementos={<ListaElementos/>}/>
                 </div>
             </div>
         </div>
@@ -56,21 +43,17 @@ export default function Profile(){//
 
 
 
-
-
-function ProfileHeader(){
-    return (
-    <>
-        <img src={star} className="Star-Prof" alt="Estrella" />
-        <img src={logo} className='App-logo-Prof' alt='logo'/>
-        Costructora Uroboros
-        <Options className='opciones-button'/>
-    </>
-);
-}
-
+/*
+*------------------COLUMNA UNO: MUESTRA LOS DATOS DEL USUARIO-----------------
+*/
 function ProfileColumnOne({info}){
-    const dato = usuariosList.find(u => u.username === info);
+
+  const navigate = useNavigate();
+  const update = ()=>{navigate('/actualizar');};
+
+  let estilo_button_EditarPerfil = {float:'buttom', width: '10%', fontSize:'20px', backgroundColor:'white', color: 'rgb(66, 135, 245)', border: '0.1px solid rgb(100, 100, 100)', borderRadius: '10px'};
+  const dato = usuariosList.find(u => u.username === info);
+
     return (
         <>
             <img className='photo-profile' src={dato.foto} alt="usuario"/>
@@ -80,6 +63,7 @@ function ProfileColumnOne({info}){
             <p>{'Genero: '+dato.genero}</p>
             <p>{'Celular: '+dato.celular}</p>
             <p>{'Dirección: '+dato.adress}</p>
+            <button style={estilo_button_EditarPerfil} onClick={update}>{'\u270E'}</button>
         </>        
     );
 }
@@ -87,14 +71,13 @@ function ProfileColumnOne({info}){
 
 
 //----------COLAPPSE--------------
-
-function ButtonBar({args,title,description}) {
+/*
+*------------------COLUMNA DOS Y TRES: INFORMACION DE LAS OBRAS Y MIEMBROS-----------------
+*/
+function ButtonBar({args,title,elementos}) {
     const [isOpen, setIsOpen] = useState(false);
-  
     const toggle = () => setIsOpen(!isOpen);
-  
-    const estilo = {margin: 'auto', width: '90%', color: 'rgb(71, 71, 71)', backgroundColor: 'rgb(48, 185, 219)'};
-
+    const estilo_collapse = {margin: 'auto', width: '95%', color: 'rgb(71, 71, 71)', backgroundColor: 'rgb(158, 194, 255)'};
 
     return (
       <React.StrictMode className='buttonBar'>
@@ -102,9 +85,9 @@ function ButtonBar({args,title,description}) {
           {title}
         </Button>
         <Collapse className='card-profile' isOpen={isOpen} {...args} >
-          <Card style={estilo}>
-            <CardBody  style={estilo}>
-              {description}
+          <Card style={estilo_collapse}>
+            <CardBody  style={estilo_collapse}>
+              {elementos}
             </CardBody>
           </Card>
         </Collapse>
@@ -114,24 +97,33 @@ function ButtonBar({args,title,description}) {
 
 
 
-  //--------------DROPDOWN-------------
 
+
+
+
+
+
+
+//--------------DROPDOWN-------------
+/*
+*------------------BOTON DE OPCIONES: PERMITE VER INF O CERRAR SESION-----------------
+*/
 function Options(props) {
+  const navigate = useNavigate();
+  const cerrarSesion = ()=>{navigate('/');};
+  const acercaInf = ()=>{navigate('/acerca-de-nosotros');};
+
   const [dropdownOpen, setDropdownOpen] = useState(false);
   const toggle = () => setDropdownOpen((prevState) => !prevState);
 
-  const navigate = useNavigate();
-
-  const cerrarSesion = ()=>{navigate('/');};
-
   return (
-    <Dropdown isOpen={dropdownOpen} toggle={toggle} {...props} style={{width: '10vw', display: 'inline',float: 'right'}}>
+    <Dropdown isOpen={dropdownOpen} toggle={toggle} {...props} direction='down' style={{display: 'inline', float: 'right'}}> 
       <DropdownToggle caret size="lg" color='primary'>
         Opciones
       </DropdownToggle>
       <DropdownMenu>
         <DropdownItem header>Perfil</DropdownItem>
-        <DropdownItem>Acerca de nosotros</DropdownItem>
+        <DropdownItem onClick={acercaInf}>Acerca de nosotros</DropdownItem>
         <DropdownItem onClick={cerrarSesion}>Cerrar sesión</DropdownItem>
       </DropdownMenu>
     </Dropdown>
