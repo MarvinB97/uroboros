f03128f395f188f558c729965a19c514fa7a6bc5
MarvinB97 <103372738+MarvinB97@users.noreply.github.com>
2024-05-19 16:33:46 -0500

    Create Gerente.js Marvin 19-05-24

---
+++ Front-end/src/Gerente.js
@@ -0,0 +1,38 @@
+import {usuariosList} from './UsuariosLista.js';
+import Encabezado from './Encabezado.js';
+
+import { Button} from 'reactstrap';
+import { useNavigate } from 'react-router-dom';
+
+
+
+export default function Gerente(){
+    const navigate = useNavigate();
+    const volver = ()=>{navigate('/profile')};
+    const usur = 'Gerente';
+    
+    return (
+        <>
+            <Encabezado boton={<Button color='primary' onClick={volver} style={{fontSize: '20px', display: 'inline', float: 'right'}}>volver</Button>}/>
+            <UsuarioInfo info={usur}/>
+        </>
+    );
+}
+
+
+function UsuarioInfo({info}){
+  
+    const dato = usuariosList.find(u => u.rol === info);
+  
+      return (
+          <div style={{padding: '5vh'}}>
+              <img className='photo-profile' src={dato.foto} alt="usuario"/>
+              <h5>{dato.nombres + ' ' +dato.apellidos}</h5>
+              <h6>{'Cargo: '+dato.rol}</h6>
+              <p>{dato.tipoDoc+ ': ' +dato.documento}</p>
+              <p>{'Genero: '+dato.genero}</p>
+              <p>{'Celular: '+dato.celular}</p>
+              <p>{'Dirección: '+dato.adress}</p>
+          </div>        
+      );
+  }
